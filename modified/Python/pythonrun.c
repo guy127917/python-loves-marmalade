ff2563407bbbef7d38d1894377ea47b36054a1d4
Guy Burton <guy@beatbutchermpc.com>
2011-05-29 14:24:27 +0100

    Moved Python verbose definition to python_lib mkb rather than modified c file

--- modified/Python/pythonrun.c
+++ modified/Python/pythonrun.c
@@ -74,7 +74,7 @@ extern void _PyGILState_Fini(void);
 #endif /* WITH_THREAD */
 
 int Py_DebugFlag; /* Needed by parser.c */
-int Py_VerboseFlag=1; /* Needed by import.c */
+int Py_VerboseFlag=PYTHON_VERBOSE; /* Needed by import.c */
 int Py_InteractiveFlag; /* Needed by Py_FdIsInteractive() below */
 int Py_InspectFlag; /* Needed to determine whether to exit at SystemError */
 int Py_NoSiteFlag; /* Suppress 'import site' */
