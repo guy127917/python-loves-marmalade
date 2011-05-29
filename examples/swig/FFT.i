3d884bdbc9b2eca850af9cbecc8fa20fe5631031
Guy Burton <guy@beatbutchermpc.com>
2011-05-29 22:36:30 +0100

    SWIG example should now work with C FFT function

--- examples/swig/FFT.i
+++ examples/swig/FFT.i
@@ -1,6 +1,10 @@
-
 %module fft
 %{
+
+extern void smbFft(float *reals, float* imags, const long fftFrameSize, const long sign);
 %}
 
 void smbFft(float *reals, float* imags, const long fftFrameSize, const long sign);
+
+%include "carrays.i"
+%array_class(float, floatArray);
--- examples/swig/data/swig_test.py
+++ examples/swig/data/swig_test.py
@@ -1,9 +1,26 @@
 import fft
 import time
+import math
 
 def main():
     print "Running fast FFT in C from Python"
-    
+
+    reals = fft.floatArray(256)
+    imags = fft.floatArray(256)
+    for i in range(0, 256):
+        x = i / 256.0
+        reals[i] = math.asin(x)
+        imags[i] = 0
+
+    # call C function
+    fft.smbFft(reals, imags, 256, 1)
+
+    print "FFT complete"
+    # print magnitudes
+    for i in range(0,256):
+        real = reals[i]
+        imag = imags[i]
+        print math.sqrt(real*real + imag*imag)
 
     time.sleep(2)    
     
